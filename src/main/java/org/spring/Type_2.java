3a7967afa781f2606f1aa3bcd6aec385edb86ecd
tran.van.thien <thientv@c-unit.co.jp>
2023-08-21 09:01:21 +0700

    update type 2

--- src/main/java/org/spring/Type_2.java
+++ src/main/java/org/spring/Type_2.java
@@ -32,15 +32,13 @@ public class Type_2 {
         System.out.println("Data Type (UI1: 17; UI2: 18)");
         int dataType = scanner.nextInt();
 
-        int parentAddress = Integer.parseInt(itemName.substring(numberCha));
-
         int offset = 0;
 
         System.out.println("Item Name,IsGroupMember,Item Enabled,Variable Enabled,Variable Name,Variable Option,Variable Class,Variable Object Name,Variable Object Option,Class Hierarchy,File Object Delimiter,Description Enabled,Description,Attribute,Link Item,Initialize Enabled,Initialize Data,Initialize Type,Force to Change,Use Empty,Deactivate,Use History,OnChange Events,Deadband Enabled,Deadbnad Min Value,Deadbnad Max Value,Filter Enabled,Filter Min Value,Filter Max Value,Chattering Enabled,Chattering Max Value,Mask Enabled,Mask Value,BCD Enabled,Request Enabled,Request Type,Request Enabled (Write),Request Type (Write),Allowed Min Value,Allowed Max Value,Allowed Step Value,Allowed List,Allowed Default,Misc Data Type,BLOB Size,BLOB Text,EU Unit,Compare Arrays,Extract Linking,Item Type,Link Property,Sub Item,Sub Item Block Size,Sub Item Block Update,Sub Item Parent Name,Sub Item Offset,Sub Item Data Type,Sub Item Unit Size,Sub Item Elem Count,Sub Item Byte Order,Direct Reading,Direct Reading Suppressed Time,Sub Item Offset Unit,Sub Item Bit,Sub Item Bit Position,Parent Item Data Type,Parent Item Unit Size,Parent Item Elem Count,Not Reconnect Judge Item,Server Publishing Setting\n");
 
         for (int i = 0; i < dataTrain.size(); i++) {
-            int itemAddress = parentAddress + i;
-            String item = itemName.substring(0, numberCha) + itemAddress;
+
+            String item = dataTrain.get(i).substring(0,dataTrain.get(i).lastIndexOf("-"));
 
             int bit = Integer.parseInt(dataTrain.get(i).substring(dataTrain.get(i).length() - 1));
 
