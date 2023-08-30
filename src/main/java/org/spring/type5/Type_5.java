ea492c9025f8627d2153245c58217f1df0e70566
tran.van.thien <thientv@c-unit.co.jp>
2023-08-30 10:35:43 +0700

    update type 5

--- src/main/java/org/spring/type5/Type_5.java
+++ src/main/java/org/spring/type5/Type_5.java
@@ -33,10 +33,14 @@ public class Type_5 {
         System.out.println("Item's Name: " + parent.getAddress());
 
         System.out.println("Item Name,IsGroupMember,Item Enabled,Variable Enabled,Variable Name,Variable Option,Variable Class,Variable Object Name,Variable Object Option,Class Hierarchy,File Object Delimiter,Description Enabled,Description,Attribute,Link Item,Initialize Enabled,Initialize Data,Initialize Type,Force to Change,Use Empty,Deactivate,Use History,OnChange Events,Deadband Enabled,Deadbnad Min Value,Deadbnad Max Value,Filter Enabled,Filter Min Value,Filter Max Value,Chattering Enabled,Chattering Max Value,Mask Enabled,Mask Value,BCD Enabled,Request Enabled,Request Type,Request Enabled (Write),Request Type (Write),Allowed Min Value,Allowed Max Value,Allowed Step Value,Allowed List,Allowed Default,Misc Data Type,BLOB Size,BLOB Text,EU Unit,Compare Arrays,Extract Linking,Item Type,Link Property,Sub Item,Sub Item Block Size,Sub Item Block Update,Sub Item Parent Name,Sub Item Offset,Sub Item Data Type,Sub Item Unit Size,Sub Item Elem Count,Sub Item Byte Order,Direct Reading,Direct Reading Suppressed Time,Sub Item Offset Unit,Sub Item Bit,Sub Item Bit Position,Parent Item Data Type,Parent Item Unit Size,Parent Item Elem Count,Not Reconnect Judge Item,Server Publishing Setting\n");
+        int offset = 0;
 
         for (Model model : models) {
-            double offset = model.getSize() * 2;
-            System.out.println("WordItem" + parent.getAddress() + "|" + model.getAddress() + ",False,True,False,,,0,,,False,\\,True,,0,,False,,3,0,False,False,True,False,False,0,0,False,0,0,False,0,False,0,False,False,12,False,12,,,,,,,,,,True,False,0,0,True,0,0,WordItem" + model.getAddress() + "," + (int) offset + "," + getDataType(model).getValue() + "," + getDataSize(model) + ",1,1,False,0,0,False,0,0,0,0,False,2\n");
+            offset = offset * 2;
+            System.out.println("WordItem" + parent.getAddress() + "|" + model.getAddress() + ",False,True,False,,,0,,,False,\\,True,,0,,False,,3,0,False,False,True,False,False,0,0,False,0,0,False,0,False,0,False,False,12,False,12,,,,,,,,,,True,False,0,0,True,0,0,WordItem" + model.getAddress() + "," + offset + "," + getDataType(model).getValue() + "," + getDataSize(model) + ",1,1,False,0,0,False,0,0,0,0,False,2\n");
+            if (offset == 0) {
+                offset = 1;
+            }
         }
     }
 
@@ -70,7 +74,7 @@ public class Type_5 {
     }
 
     private static List<Model> getModels() throws IOException {
-        FileInputStream file = new FileInputStream(new File("D:/abc.xlsx"));
+        FileInputStream file = new FileInputStream(new File("D:/inputSheet.xlsx"));
         Workbook workbook = new XSSFWorkbook(file);
 
         Sheet sheet = workbook.getSheetAt(0);
