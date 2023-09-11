9b269ebcda2f0b68c0ec3dd9e4db423a6e688966
tran.van.thien <thientv@c-unit.co.jp>
2023-09-11 15:00:33 +0700

    update

--- src/main/java/org/spring/Type_4.java
+++ src/main/java/org/spring/Type_4.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Scanner;
 
 public class Type_4 {
     public static void main(String[] args) {
@@ -25,7 +24,20 @@ public class Type_4 {
                         "F4\n" +
                         "F5\n" +
                         "F6\n" +
-                        "F7\n";
+                        "F7\n" +
+                        "F8\n" +
+                        "F9\n" +
+                        "F10\n" +
+                        "F11\n" +
+                        "F12\n" +
+                        "F13\n" +
+                        "F14\n" +
+                        "F15\n" +
+                        "F16\n" +
+                        "F17\n" +
+                        "F20\n" +
+                        "F21\n";
+
 
         List<String> data = new ArrayList<>(Arrays.asList(input.split("\n")));
         Collections.sort(data, Collator.getInstance());
@@ -33,31 +45,29 @@ public class Type_4 {
 
         System.out.println("Item Name,IsGroupMember,Item Enabled,Variable Enabled,Variable Name,Variable Option,Variable Class,Variable Object Name,Variable Object Option,Class Hierarchy,File Object Delimiter,Description Enabled,Description,Attribute,Link Item,Initialize Enabled,Initialize Data,Initialize Type,Force to Change,Use Empty,Deactivate,Use History,OnChange Events,Deadband Enabled,Deadbnad Min Value,Deadbnad Max Value,Filter Enabled,Filter Min Value,Filter Max Value,Chattering Enabled,Chattering Max Value,Mask Enabled,Mask Value,BCD Enabled,Request Enabled,Request Type,Request Enabled (Write),Request Type (Write),Allowed Min Value,Allowed Max Value,Allowed Step Value,Allowed List,Allowed Default,Misc Data Type,BLOB Size,BLOB Text,EU Unit,Compare Arrays,Extract Linking,Item Type,Link Property,Sub Item,Sub Item Block Size,Sub Item Block Update,Sub Item Parent Name,Sub Item Offset,Sub Item Data Type,Sub Item Unit Size,Sub Item Elem Count,Sub Item Byte Order,Direct Reading,Direct Reading Suppressed Time,Sub Item Offset Unit,Sub Item Bit,Sub Item Bit Position,Parent Item Data Type,Parent Item Unit Size,Parent Item Elem Count,Not Reconnect Judge Item,Server Publishing Setting");
 
-        Scanner scanner = new Scanner(System.in);
-
         String parentItem = dataTrain.get(0);
         int offset = 0;
 
         for (int i = 0; i < dataTrain.size(); i++) {
 
             int address = Integer.parseInt(dataTrain.get(i).replaceAll("\\D+", ""));
-            if (i != 0) {
-
-                int previousAddress = Integer.parseInt(dataTrain.get(i - 1).replaceAll("\\D+", ""));
-
-                if (address - 1 != previousAddress) {
-                    parentItem = dataTrain.get(i);
+            if (i + 1 < dataTrain.size()) {
+                int nextAddress = Integer.parseInt(dataTrain.get(i + 1).replaceAll("\\D+", ""));
+                if (address + 1 != nextAddress) {
+                    int count = offset / 2 + 1;
+                    System.out.println("WordItem" + parentItem + ",False,True,True," + parentItem + ",'ELEM=" + count + ",VT=Bit',0,,,False,\\,True,,1,,False,,3,0,False,False,True,False,False,0,0,False,0,0,False,0,False,0,False,False,12,False,12,,,,,,,,,,True,False,0,0,False,99,0,,0,0,0,0,0,False,0,0,False,0,8209,1," + count + ",False,2");
+                    System.out.println("WordItem" + parentItem + "|" + dataTrain.get(i) + ",False,True,False,,,0,,,False,\\,True,,0,,False,,3,0,False,False,True,False,False,0,0,False,0,0,False,0,False,0,False,False,12,False,12,,,,,,,,,,True,False,0,0,True,0,0,WordItem" + parentItem + "," + offset + ",17,1,1,1,False,0,0,False,0,0,0,0,False,2");
+                    parentItem = dataTrain.get(i + 1);
                     offset = 0;
                 } else {
-                    if (address - 1 != previousAddress) {
-                        int count = offset / 2 + 1;
-                        System.out.println("WordItem" + parentItem + ",False,True,True," + parentItem + ",'ELEM=" + count + ",VT=Bit',0,,,False,\\,True,,1,,False,,3,0,False,False,True,False,False,0,0,False,0,0,False,0,False,0,False,False,12,False,12,,,,,,,,,,True,False,0,0,False,99,0,,0,0,0,0,0,False,0,0,False,0,8209,1," + count + ",False,2");
-                    }
                     System.out.println("WordItem" + parentItem + "|" + dataTrain.get(i) + ",False,True,False,,,0,,,False,\\,True,,0,,False,,3,0,False,False,True,False,False,0,0,False,0,0,False,0,False,0,False,False,12,False,12,,,,,,,,,,True,False,0,0,True,0,0,WordItem" + parentItem + "," + offset + ",17,1,1,1,False,0,0,False,0,0,0,0,False,2");
                     offset++;
                 }
-            } else {
-                System.out.println("WordItem" + parentItem + ",False,True,True," + parentItem + ",'ELEM=99,VT=Bit',0,,,False,\\,True,,1,,False,,3,0,False,False,True,False,False,0,0,False,0,0,False,0,False,0,False,False,12,False,12,,,,,,,,,,True,False,0,0,False,99,0,,0,0,0,0,0,False,0,0,False,0,8209,1,99,False,2");
+            }
+            if (i == dataTrain.size() - 1) {
+                int count = offset / 2 + 1;
+                System.out.println("WordItem" + parentItem + ",False,True,True," + parentItem + ",'ELEM=" + count + ",VT=Bit',0,,,False,\\,True,,1,,False,,3,0,False,False,True,False,False,0,0,False,0,0,False,0,False,0,False,False,12,False,12,,,,,,,,,,True,False,0,0,False,99,0,,0,0,0,0,0,False,0,0,False,0,8209,1," + count + ",False,2");
+                System.out.println("WordItem" + parentItem + "|" + parentItem + ",False,True,False,,,0,,,False,\\,True,,0,,False,,3,0,False,False,True,False,False,0,0,False,0,0,False,0,False,0,False,False,12,False,12,,,,,,,,,,True,False,0,0,True,0,0,WordItem" + parentItem + ",0,17,1,1,1,False,0,0,False,0,0,0,0,False,2");
             }
         }
     }
